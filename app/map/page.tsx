d9ddaf5eba271c7f88efedc5768fc39677d30476
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2025-02-22 22:47:50 +0900

    🚀 FEAT: 새로운 기능 추가
    개요 페이지 구현

---
+++ app/map/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Map() {
+  return (
+    <div className={styles.map}>
+      <h1 className={styles.map_title}>MAP</h1>
+    </div>
+  );
+}
