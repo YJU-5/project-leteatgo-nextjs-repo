7f229d022bba9124c2bf0cc8bb3b790bc1d19c95
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2025-02-24 16:01:22 +0900

    add start/join start/create

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,18 +1,24 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Start() {
   return (
     <div className={styles.start}>
       <div className={styles.top_container}>
         <div className={styles.button_group}>
-          <button className={styles.create_room}>개최하기</button>
-          <button className={styles.join_room}>참가하기</button>
+          <Link href="/start/create" className={styles.create_room}>
+            개최하기
+          </Link>
+          <Link href="/start/join" className={styles.join_room}>
+            참가하기
+          </Link>
         </div>
       </div>
       <hr />
       <div className={styles.under_container}>
         <h1 className={styles.popular_title}>현재 인기 소셜 다이닝</h1>
         <div className={styles.popular_item}>아잉</div>
+        <div className={styles.popular_item}></div>
         <div className={styles.popular_item}></div>
         <div className={styles.popular_item}></div>
       </div>
