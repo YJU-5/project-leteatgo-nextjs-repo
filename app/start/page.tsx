6fda239c3d5ae4f31aa567ffe53291409af782c0
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2025-02-24 15:43:04 +0900

    시작하기 css 수정

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -3,7 +3,19 @@ import styles from "./page.module.css";
 export default function Start() {
   return (
     <div className={styles.start}>
-      <h1 className={styles.start_title}>시작하기</h1>
+      <div className={styles.top_container}>
+        <div className={styles.button_group}>
+          <button className={styles.create_room}>개최하기</button>
+          <button className={styles.join_room}>참가하기</button>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.under_container}>
+        <h1 className={styles.popular_title}>현재 인기 소셜 다이닝</h1>
+        <div className={styles.popular_item}>아잉</div>
+        <div className={styles.popular_item}></div>
+        <div className={styles.popular_item}></div>
+      </div>
     </div>
   );
 }
