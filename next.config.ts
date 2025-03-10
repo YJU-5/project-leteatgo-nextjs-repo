a882fb8b4cceb554d0cae69d5291f187bb16da59
SeungHyun CHA <cjhms002@naver.com>
2025-03-10 11:06:53 +0900

    edit/swc style/home

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import { Configuration } from "webpack";
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+    styledComponents: true,
+    emotion: true,
+    reactRemoveProperties: true,
+  },
   webpack: (config: Configuration) => {
-    // 기존 webpack 설정 유지
     if (!config.optimization) {
       config.optimization = {};
-    }
-
-    if (!config.optimization.splitChunks) {
-      config.optimization.splitChunks = {};
     }
 
     config.optimization.splitChunks = {
