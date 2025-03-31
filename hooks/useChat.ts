90f30c5ce3bfb2ef00c1b7adcf2eb650f273cf7f
KimHyeongSun445 <gudtjs1004sd@naver.com>
2025-03-31 09:09:23 +0900

    🚀 FEAT: 새로운 기능 추가
    웹 소캣에 관련된 이벤트 추가
     #15

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,50 +1,60 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3001/chat-room/join',{
-    transports:['websocket'], // WebSocket만 사용 (보안 Http 풀링방지)
-    withCredentials:true,
-    auth:{
-        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imd1ZHRqczEwMDRzZEBnbWFpbC5jb20iLCJuYW1lIjoi6rmA7ZiV7ISgIiwic29jaWFsSWQiOiIzOTM1MzI5NDc3IiwiZGVsZXRlZCI6ZmFsc2UsImlhdCI6MTc0MjA5NTQ1NiwiZXhwIjoxNzQyMzk3ODU2fQ.RZ8KFE3SeyjLC2ijeFdgqzNNj6k2eeDMStQeRxQh6VM' // localStorage.getItem('token')
+const token = localStorage.getItem('jwtToken')
+
+const socket = io('http://localhost:3001/chat-room/join', {
+    transports: ['websocket'], // WebSocket만 사용 (보안 Http 풀링방지)
+    withCredentials: true,
+    auth: {
+        token: token
     }
 })
-
-// 연결 종료 감지 
-socket.on('disconnect',(reason)=>{
-    console.log(`🔴 WebSocket 연결이 끊어졌습니다. 이유: ${reason}`);
-})
-
-// 웹소캣 에러 감지 
-socket.on('connect_error',(error)=>{
-    console.error('❌ WebSocket 연결 오류:', error.message);
-})
-
-export const useChat = (roomId:string)=>{
+export const useChat = (roomId: string) => {
     const [messages, setMessages] = useState<string[]>([])
+    const [participants, setParticipants] = useState([])
 
-    useEffect(()=>{
-        
+    console.log(participants);
+
+    useEffect(() => {
         // 채팅방 참여 
-        socket.emit('joinRoom',roomId)
+        socket.emit('joinRoom', roomId)
 
-        // 메세지 수신
-        socket.on('message',(msg)=>{
-            console.log(msg);
-            setMessages((prev)=>[...prev,msg])
+        // 채팅목록 가져오기 
+        socket.emit('getMessages',roomId)
+
+        // 채팅방 참여자 목록 가져오기 이벤트 요청  
+        socket.emit('getRoomParticipants',roomId)
+
+        // 서버에서 응답받은 참여자 목록 
+        // 이름, 역할, 사진, 자기소개, id 
+        // @@@@@@@@@@@@@@@@@@@@
+        socket.on('roomParticipants',(users)=>{
+            console.log(users);
         })
 
-        // 채팅방 나가기 
-        return()=>{
-            socket.emit('leaveRoom',roomId)
-            socket.off('message')
-        }
-    },[roomId])
+        socket.on('messages',(msg)=>{
+            setMessages((prev)=>[...prev, ...msg])
+        })
+
+        // 메세지 수신
+        socket.on('message', (msg) => {
+            console.log(msg);
+            setMessages((prev) => [...prev, msg])
+        })
+        
+    }, [roomId])
 
     // 메세지 보내기 
-    const sendMessage = (msg:string)=>{
-        socket.emit('message',{roomId,message:msg})
+    const sendMessage = (msg: string) => {
+        socket.emit('message', { roomId, message: msg })
     }
 
-    return {messages, sendMessage}
+    // 채팅방 나가기 나가고 나서 메인페이지로
+    const leaveRoom=()=>{
+        socket.emit('leaveRoom',{ roomId })
+    }
+
+    return { messages, sendMessage, leaveRoom }
 
 }
