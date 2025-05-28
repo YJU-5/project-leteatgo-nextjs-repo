54fa1bc638c2d441b81a56ffde3d086e649383d6
SeungHyun CHA <cjhms002@naver.com>
2025-05-28 09:13:07 +0900

    style/chat-room

--- app/chat-room/[roomId]/page.tsx
+++ app/chat-room/[roomId]/page.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect, useRef } from "react";
 import { useParams } from "next/navigation";
 import { useChat } from "@/hooks/useChat";
 import Image from "next/image";
+import Description from "@/components/Description/Description";
 
 interface ChatMessage {
   id?: string;
@@ -15,10 +16,51 @@ interface ChatMessage {
   isCurrentUser: boolean;
 }
 
+interface Host {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string | null;
+  birthday: string | null;
+  gender: string | null;
+  pictureUrl: string;
+  description: string | null;
+  role: string;
+  socialProvider: string;
+  socialId: string;
+  createdAt: string;
+  updatedAt: string;
+  deleted: boolean;
+}
+
+interface Content {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  startDate: string;
+  maxParticipants: number;
+  gender: string;
+  pictureUrl: string;
+  minAge: number;
+  maxAge: number;
+  latitude: string;
+  longitude: string;
+  address: string;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+  isActive: number;
+  profileImg: string;
+  username: string;
+  name: string;
+  hostId: Host;
+}
+
 export default function ChatRoomPage() {
   const { roomId } = useParams();
-  const [roomInfo, setRoomInfo] = useState({
-    id: "",
+  const [roomInfo, setRoomInfo] = useState<Content>({
+    id: 0,
     title: "",
     description: "",
     status: "",
@@ -35,12 +77,34 @@ export default function ChatRoomPage() {
     createdAt: "",
     updatedAt: "",
     isActive: 0,
+    profileImg: "",
+    username: "",
+    name: "",
+    hostId: {
+      id: "",
+      name: "",
+      email: "",
+      phoneNumber: null,
+      birthday: null,
+      gender: null,
+      pictureUrl: "",
+      description: null,
+      role: "",
+      socialProvider: "",
+      socialId: "",
+      createdAt: "",
+      updatedAt: "",
+      deleted: false,
+    },
   });
+
   const { messages, sendMessage, userList } = useChat(roomId as string);
   const [input, setInput] = useState("");
   const [isComposing, setIsComposing] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const [showSidePanel, setShowSidePanel] = useState(false);
+  const [selectedContent, setSelectedContent] = useState<Content | null>(null);
+  const { leaveRoom } = useChat(roomId as string);
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -68,7 +132,7 @@ export default function ChatRoomPage() {
       } catch (error) {
         console.error("소셜다이닝 데이터를 가져오는데 실패했습니다:", error);
         setRoomInfo({
-          id: "",
+          id: 0,
           title: "",
           description: "",
           status: "",
@@ -85,6 +149,25 @@ export default function ChatRoomPage() {
           createdAt: "",
           updatedAt: "",
           isActive: 0,
+          profileImg: "",
+          username: "",
+          name: "",
+          hostId: {
+            id: "",
+            name: "",
+            email: "",
+            phoneNumber: null,
+            birthday: null,
+            gender: null,
+            pictureUrl: "",
+            description: null,
+            role: "",
+            socialProvider: "",
+            socialId: "",
+            createdAt: "",
+            updatedAt: "",
+            deleted: false,
+          },
         });
       }
     };
@@ -98,6 +181,13 @@ export default function ChatRoomPage() {
 
   return (
     <div className={styles.chatRoom}>
+      {selectedContent && (
+        <Description
+          content={selectedContent}
+          onClose={() => setSelectedContent(null)}
+          link={false}
+        />
+      )}
       <div className={styles.chatRoomHeader}>
         <h1 className={styles.chatRoomTitle}>Chat Room: {roomInfo.title}</h1>
         <Image
@@ -167,41 +257,44 @@ export default function ChatRoomPage() {
           }`}
         >
           <div className={styles.sidePanelContent}>
-            <p className={styles.sidePanelInfo}>소셜다이닝 정보</p>
+            <p
+              className={styles.sidePanelInfo}
+              onClick={() => setSelectedContent(roomInfo)}
+            >
+              소셜다이닝 정보
+            </p>
+            <hr
+              style={{
+                margin: "10px 0",
+                border: "0.5px solid #fff",
+              }}
+            />
             <p className={styles.sidePanelInfo}>참여자 정보</p>
             {userList.map((user) => (
-              <p key={user.id} className={styles.sidePanelInfo}>
+              <div key={user.id} className={styles.sidePanelProfile}>
                 {user.userId.pictureUrl ? (
                   <Image
                     src={user.userId.pictureUrl}
+                    className={styles.sidePanelProfileImg}
                     alt="profile"
                     width={50}
                     height={50}
                   />
                 ) : null}
-                {user.userId.name}
-              </p>
+                <p className={styles.sidePanelProfileName}>
+                  {user.userId.name}
+                </p>
+              </div>
             ))}
-            <p className={styles.sidePanelInfo}>방 나가기</p>
-
-            {/* <p className={styles.sidePanelTitle}>Title: {roomInfo.title}</p>
-            <p className={styles.sidePanelDescription}>
-              Description: {roomInfo.description}
+            <hr
+              style={{
+                margin: "10px 0",
+                border: "0.5px solid #fff",
+              }}
+            />
+            <p className={styles.sidePanelInfo} onClick={() => leaveRoom()}>
+              방 나가기
             </p>
-            <p className={styles.sidePanelStatus}>Status: {roomInfo.status}</p>
-            <p className={styles.sidePanelStartDate}>
-              Start Date: {roomInfo.startDate}
-            </p>
-            <p className={styles.sidePanelMaxParticipants}>
-              Max Participants: {roomInfo.maxParticipants}
-            </p>
-            <p className={styles.sidePanelGender}>Gender: {roomInfo.gender}</p>
-            <p className={styles.sidePanelMinAge}>Min Age: {roomInfo.minAge}</p>
-            <p className={styles.sidePanelMaxAge}>Max Age: {roomInfo.maxAge}</p>
-            <p className={styles.sidePanelAddress}>
-              Address: {roomInfo.address}
-            </p>
-            <p className={styles.sidePanelPrice}>Price: {roomInfo.price}</p> */}
           </div>
         </div>
       )}
