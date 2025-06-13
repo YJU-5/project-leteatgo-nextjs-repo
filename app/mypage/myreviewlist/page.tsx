2b604c563680a32c602b3b7af36aac21dfa72d4f
jinmo550 <eorn8110@g.yju.ac.kr>
2025-06-13 11:08:20 +0900

    fetch 수정

--- app/mypage/myreviewlist/page.tsx
+++ app/mypage/myreviewlist/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import EventList from "@/components/MyPage/EventList/EventList";
 
@@ -10,58 +10,31 @@ interface Review {
   date: string;
   pictureUrl: string;
   address: string;
-  chatRoomId:string;
+  chatRoomId: string;
   createdAt: string;
 }
 
 
 export default function MyReviewList() {
 
-  const [reviews,setReviews]= useState<Review[]>([]);
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
-      async function getEvents() {
-        const res = await fetch("http://localhost:3001/review/userreview", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
-          },
-        });
-        if (!res.ok) throw new Error("Failed to fetch events");
-        const data = await res.json();
-        setReviews(data);
-      }
-      getEvents();
-    }, []);
-    console.log('reviews',reviews);
-
-  // const reviews: Review[] = [
-  //   {
-  //     id: 1,
-  //     title: "리뷰 제목 1",
-  //     date: "2024.10.15",
-  //     pictureUrl: "/gitb.png",
-  //     address: "서울특별시 강남구 테헤란로",
-  //     createdAt:'2025-03-01'
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "리뷰 제목 2",
-  //     date: "2024.11.05",
-  //     pictureUrl: "/gitb.png",
-  //     address: "경기도 성남시 분당구 정자동",
-  //     createdAt:'2025-03-01'
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "리뷰 제목 3",
-  //     date: "2024.12.20",
-  //     pictureUrl: "/gitb.png",
-  //     address: "부산광역시 해운대구",
-  //     createdAt:'2025-03-01'
-  //   },
-  // ];
+    async function getEvents() {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review/userreview`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+        },
+      });
+      if (!res.ok) throw new Error("Failed to fetch events");
+      const data = await res.json();
+      setReviews(data);
+    }
+    getEvents();
+  }, []);
+  console.log('reviews', reviews);
 
   const total = reviews.length;  //개시물의 총 개수
 
