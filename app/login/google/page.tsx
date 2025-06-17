71d04862f6a67cec5b21fa0540b2ee0371b393dd
jinmo550 <eorn8110@g.yju.ac.kr>
2025-06-17 15:49:23 +0900

    프로필 수정

--- app/login/google/page.tsx
+++ app/login/google/page.tsx
@@ -100,7 +100,7 @@ export default function GoogleCallback() {
 =======
         //백엔드로 accessToken 보내주기
         const response = await fetch(
-          "http://localhost:3001/user/google/login",
+          `${process.env.NEXT_PUBLIC_API_URL}/user/google/login`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
